d15561acfca28b4466cac82d7079cba0465768cc
Duong Dinh Cuong <cuong3ihut@gmail.com>
2019-06-08 21:04:12 +0200

    Update demo runDiagnostic() with UI interaction

--- main.js
+++ main.js
@@ -1,57 +1,101 @@
 const DecisionTree = require("./lib/decision-tree-json").default
 const decisionTree = new DecisionTree(__dirname + "/database", "ROOT.json")
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-function showOpts(index, opt) {
-  var tabs = ''; for (i = 0; i < index; i++) tabs += ' '
-  console.log(tabs + ' - Option ' + opt.key + ' (' + opt.label + ')')
+function flatten(list) {
+  return list.reduce(function (a, b) {
+    return a.concat(Array.isArray(b) ? flatten(b) : b);
+  }, []).filter(function (elem) {
+    return typeof elem !== "undefined" && elem != null;
+  });
 }
+
+function showOpts(index, opt, optidx) {
+  var tabs = ''; for (i = 0; i < index; i++) tabs += ' '
+  console.log(tabs + ' [' + optidx + ']. Option ' + opt.key + ' (' + opt.label + ')')
+}
+
 function showGHMs(index, tree) {
   var tabs = ''; for (i = 0; i < index; i++) tabs += ' '
   var lastleafs = tree.lastleafs()
   var GHMs = lastleafs.length ? lastleafs.join() : tree.current().key
   console.log(tabs + index + '. ' + tree.lastsel().key + ' *GHMs: (' + GHMs + ')')
 }
+
 function getKeyValue(params) {
   return Object.keys(params).map(key => {
     return key + "=" + params[key]
   })
 }
+
 function diagnostic(index, arrays, params) {
   let last = null
-  arrays.forEach(pos => {
+  var options = arrays.map(pos => {
+    var options = null;
     if (last) {
       decisionTree.set(last, pos).next(params)
       showGHMs(index, decisionTree)
       var current = decisionTree.current()
-      current.options ? current.options.forEach(opt => showOpts(index, opt)) : null
+      options = current.options ? current.options.map((opt, idx) => {
+        showOpts(index, opt, idx)
+        return opt.key
+      }) : []
       index++
     }
     last = pos
+    return options
   })
-  return index
+  return {
+    options: options,
+    index: index
+  }
 }
+
+function startDiagnostic(index) {
+  var current = decisionTree.current()
+  return current.options.map((opt, idx) => {
+    showOpts(index, opt, idx)
+    return opt.key
+  })
+}
+
 function showResult() {
   console.log("")
   console.log(" HISTORY:", decisionTree.history().join(', '))
   console.log(" JOURNEY:", decisionTree.journey().map(trace => trace.params ? trace.key + '(' + getKeyValue(trace.params) + ')' : trace.key).join(', '))
   console.log("")
 }
+
+function runDiagnostic(last, options, index, params) {
+  reader.question('\nWhich option will you process? Choose from [0..' + (options.length-1) + ']: ', (idx) => {
+    const transition = flatten([last, options[parseInt(idx)]])
+    const result = diagnostic(index, transition, params)
+    const nextOptions = flatten(result.options)
+    const nextNode = transition[1]
+    if (nextOptions.length) {
+      runDiagnostic(nextNode, nextOptions, result.index, params)
+    } else {
+      showResult()
+      reader.close();
+    }
+  })
+}
+
 function main() {
   try {
-    var index = 1
-    index = diagnostic(index, ['ROOT', 'CMD-28', 'D-145', 'D-143', 'A-205'], { sessions: 1, age: 28 })
-    // back to once node
-    decisionTree.prev(); index--
-    index = diagnostic(index, ['D-143', 'A-170'])
-    showResult()
-    // new session with unterminated node
-    decisionTree.reset(); index = 1
-    index = diagnostic(index, ['ROOT', 'CMD-28', 'D-145'], { sessions: 1, age: 28 })
-    showResult()
-    // new session with NULL termiated condition
-    decisionTree.reset(); index = 1
-    index = diagnostic(index, ['ROOT', 'CMD-28', 'D-145', 'D-143', 'A-342'], { sessions: 1, age: 28 })
-    showResult()
+    var index = 0
+    var doctorPath = ['ROOT']
+    reader.question('Number of session? ', (nbrsession) => {
+      reader.question('How old are your? ', (age) => {
+        const params = { sessions: nbrsession, age: age }
+        const cmdOptions = startDiagnostic(index++, params)
+        runDiagnostic(doctorPath, flatten(cmdOptions), index, params)
+      })
+    });
   } catch (ex) {
     console.log(ex.message)
   }
